b185ae78f89856d3d91e9819ca625fc5b1e97c66
Hung Vuong <hungkha61@gmail.com>
2026-03-29 01:14:28 +0700

    update script.js fix UI

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,16 @@
 const cloudinary = require("cloudinary").v2;
 
+// cloudinary.config({
+//   cloud_name: "dm1n8bthw",
+//   api_key: "58145134556178",
+//   api_secret: "PO7ZuALAcFiNxBd7YBW7IsGEUmY"
+// });
+
+
 cloudinary.config({
-  cloud_name: "dm1n8bthw",
-  api_key: "58145134556178",
-  api_secret: "PO7ZuALAcFiNxBd7YBW7IsGEUmY"
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 module.exports = cloudinary;
