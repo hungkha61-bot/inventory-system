808131286de3346f8b9863f388ac0907d8ca9fcb
Hung Vuong <hungkha61@gmail.com>
2026-04-03 14:50:44 +0700

    update script.js fix UI

--- frontend/admin.js
+++ frontend/admin.js
@@ -2,6 +2,7 @@ const API = "https://inventory-system-syzl.onrender.com/api";
 
 // 🔒 Protect admin page
 const role = localStorage.getItem("role");
+const token = localStorage.getItem("token");
 
 if (role !== "admin") {
   window.location.href = "store.html";
@@ -10,11 +11,18 @@ if (role !== "admin") {
 // ---------------- DASHBOARD ----------------
 async function loadDashboard() {
   try {
-    const itemsRes = await fetch(`${API}/items`);
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
+
+    const itemsRes = await fetch(`${API}/items`, { headers });
     const items = await itemsRes.json();
 
-    const ordersRes = await fetch(`${API}/orders`);
+    const ordersRes = await fetch(`${API}/orders`, { headers });
     const orders = await ordersRes.json();
+
+    console.log("ITEMS:", items);
+    console.log("ORDERS:", orders);
 
     document.getElementById("totalProducts").textContent = items.length;
     document.getElementById("totalOrders").textContent = orders.length;
